2da28163fa8caffd8ab070436baec276b4477eb1
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-11-16 16:11:26 +0000

    Add write functions

--- src/isel/leic/lic/g2/LCD/LCD.java
+++ src/isel/leic/lic/g2/LCD/LCD.java
@@ -1,6 +1,5 @@
 /*-
  * SPDX-License-Identifier: BSD-3-Clause
- *
  * Copyright (c) 2019 Luis Bandarra <luis.bandarra@homestudio.pt>
  */
 package isel.leic.lic.g2.LCD;
@@ -15,36 +14,42 @@ public class LCD {
     // Define se a interface com o LCD é série ou paralela
     private static final boolean SERIAL_INTERFACE = false;
 
-    // MASK for sending nibble (parallel)
+    // Define comandos para o LCD
+    private static final int LCD_CLEAR = 0x1;
+
+    // Mascara para envio do nibble em parallel
     private static int MASK_ENABLE = 0x20;  // 0010 0000
-    private static int MASK_RS = 0x10;      // 000R 0000
+    private static int MASK_PARALLEL_RS = 0x10;      // 000R 0000
     private static int MASK_LOW_DATA = 0x0f;    // 0000 1111
     private static int MASK_HIGH_DATA = 0xf0;    // 0000 1111
 
-    private static boolean DEBUG = false;
+    private static boolean DEBUG = true;
 
     // Escreve um nibble de comando/dados no LCD em paralelo
-    // the HAL signal will be: 0 0 E (RS) NIBBLE
-    // following the time progressing from datasheets
+    // O sinal para a HAL vai ser: 0 0 E (RS) NIBBLE
+    // seguindo o diagrama temporal da datasheet do equipamento
     private static void writeNibbleParallel(boolean rs, int data) {
-        if (rs)
-            HAL.setBits(MASK_RS);
+        if (rs == true)
+            HAL.setBits(MASK_PARALLEL_RS);
         else
-            HAL.clrBits(MASK_RS);
+            HAL.clrBits(MASK_PARALLEL_RS);
+
         HAL.setBits(MASK_ENABLE);
         HAL.writeBits(MASK_LOW_DATA, data);
         HAL.clrBits(MASK_ENABLE);
-        HAL.clrBits(MASK_RS);
+        HAL.clrBits(MASK_PARALLEL_RS);
     }
 
     // Escreve um nibble de comando/dados no LCD em série
-    // we use the SerialEmitter to send the signal
-    // the bits to send are 000R NIBBLE
+    // O sinal de dados para o SerialEmitter vai ser:
+    // 0 0 0 (D3) (D2) (D1) (D0) (RS) de acordo com a especificao projeto
     private static void writeNibbleSerial(boolean rs, int data) {
-        int value = data & MASK_LOW_DATA;   // validate set of data
-        if (rs)
-            value |= MASK_RS;               // set bit for RS
-        SerialEmitter.send(SerialEmitter.Destination.LCD, value);
+        System.out.println("Dados a enviar: " + data);
+        data = (data & MASK_LOW_DATA) << 1;
+        System.out.println("Shift: " + data);
+        if (rs == true)
+            data |= 0x1;
+        SerialEmitter.send(SerialEmitter.Destination.LCD, data);
     }
 
     // Escreve um nibble de comando/dados no LCD
@@ -57,11 +62,9 @@ public class LCD {
 
     // Escreve um byte de comando/dado no LCD
     private static void writeByte(boolean rs, int data) {
-        // write higher nibble first
         writeNibble (rs, (MASK_HIGH_DATA & data) >> 4);
-        // write lower nibble second
         writeNibble(rs, MASK_LOW_DATA & data);
-        Time.sleep(5); // wait time for execute command by lcd
+        Time.sleep(5); // espera por execucao comando lcd
     }
 
     // Escreve um comando no LCD
@@ -87,52 +90,44 @@ public class LCD {
 
         writeCMD(0x28);  // BF can be checked
         // display on/off
-        writeCMD(0x8);  // clear
+        writeCMD(0x8);  // display off
         // entry mode set
-        writeCMD(0x1);  // clear
+        writeCMD(LCD_CLEAR);  // clear
         writeCMD(0x6);  // I/D:1 inc S:0 no shift
         writeCMD(0xf); // display on
 
         System.out.println("LCD ready");
     }
 
+    // Escreve um caráter na posição corrente
+    public static void write(char c) {
+        writeDATA(c);
+    }
+
+    // Escreve uma string na posição corrente
+    public static void write(String txt) {
+        for (int i = 0; i < txt.length(); i++) {
+            writeDATA(txt.charAt(i));
+        }
+    }
+
+    // Envia comando para posicionar cursor (‘lin’:0..LINES-1 , ‘col’:0..COLS-1)
+    public static void cursor(int lin, int col) { }
+
+    // Envia comando para limpar o ecrã e posicionar o cursor em (0,0)
+    public static void clear() {
+        writeCMD(LCD_CLEAR);
+    }
+
     public static void main(String[] args) {
         HAL.init();
         init();
 
-        for (int i = 0; i < 10; i++)
-            writeDATA('0' + i);
-
-        Time.sleep(2000);
-
-        // DEBUG = true;
-        if (DEBUG) {
-            System.out.println("Testes");
-
-            System.out.println("Testes writeNibbleParallel:");
-            System.out.println("Teste 1: write RS: 0 data: 1 Result: 0010 0001 (33)");
-            writeNibbleParallel(false, 1);
-            Time.sleep(2000);
-
-            System.out.println("Teste 2: write RS: 0 data: 3 Result: 0010 0011 (35)");
-            writeNibbleParallel(false, 3);
-            Time.sleep(2000);
-
-            System.out.println("Teste 3: write RS: 1 data: 15 Result: 0011 1111 (63)");
-            writeNibbleParallel(true, 15);
-            Time.sleep(2000);
-
-            System.out.println("Teste 4: write RS: 1 data: 0 Result: 0011 0000 (48)");
-            writeNibbleParallel(true, 0);
-            Time.sleep(2000);
-
-            System.out.println("Testes writeByte:");
-            System.out.println("Teste 5: write RS: 0 data: 10 (0000 1010) Result 1: 0010 0000 (32) + 2: 0010 1010 (42)");
-            writeByte(false, 10);
-            Time.sleep(2000);
-
-            System.out.println("Teste 6: write RS: 1 data: 00 (0000 0000) Result 1: 0011 0000 (48) + 2: 0011 0000 (48)");
-            writeByte(true, 0);
-        }
+        write("Help me");
+        Time.sleep(5000);
+        clear();
+        write("SLB Campeoes");
+        clear();
+        Time.sleep(5000);
     }
 }
