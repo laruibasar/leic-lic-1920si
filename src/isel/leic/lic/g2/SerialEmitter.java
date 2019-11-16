bd8458204de31a075f011c7f3544f634b5fb4e6f
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-11-16 15:45:26 +0000

    Close SerialEmitter class

--- src/isel/leic/lic/g2/SerialEmitter.java
+++ src/isel/leic/lic/g2/SerialEmitter.java
@@ -1,31 +1,77 @@
 /*-
  * SPDX-License-Identifier: BSD-3-Clause
- *
  * Copyright (c) 2019 Luis Bandarra <luis.bandarra@homestudio.pt>
+ */
+
+/*
+ * A classe SerialEmitter envia para os varios modulos de hardware modules
+ * os dados que recebe do controlo de software.
+ *
+ * O mapeamento para o Output Port da placa vai ser mapeado de acordo com o seguinte
+ * esquema de acordo com os bits disponiveis:
+ * bit: 8   7   6   5   4   3   2    1
+ *      0   0   0   0 DOOR LCD SCLK SDX
+ * Este arranjo permite-nos, apenas com o hardware atual, ter ainda disponivel mais
+ * 4 enderecamentos disponiveis para modulos de hardware.
+ *
+ * Do Input Port da placa verificamos o sinal de Busy do hardware para fazer uma
+ * espera do envio dos dados em linha.
  */
 
 // Envia tramas para os diferentes modulos Serial Receiver
 package isel.leic.lic.g2;
 
 public class SerialEmitter {
-    private final static int TX_SDX = 0x4;
     public static enum Destination {LCD, DOOR_MECHANISM};
 
+    private final static int MASK_DATA = 0x01;
+    private final static int MASK_SCLK = 0x02;
+    private final static int MASK_LCD  = 0x04;
+    private final static int MASK_DOOR = 0x08;
+    private final static int MASK_BUSY = 0x01;
+    private final static int TX_FRAME_SIZE = 5;
+
+    private static int write_addr;
+
     // inicia a classe
-    public static void init() { }
+    public static void init() {
+        write_addr = 0;
+    }
 
-    // envia uma trama para o SerialReceiver identificando o destino em addr e os bits de dados em 'data'
-    /*
-     * Use HAL to write bits to hardware layer. Because is serial communication, we need to time the sending
-     * of information, using a mask to send the signals SS, SCLK and SDX.
-     */
+    // envia uma trama para o SerialReceiver identificando o destino em addr
+    // e os bits de dados em 'data'
     public static void send(Destination addr, int data) {
-        // build code to send
+        while (isBusy());
 
+        HAL.clrBits(0xff);
+
+        switch (addr) {
+            case LCD:
+                write_addr = MASK_LCD;
+                break;
+            case DOOR_MECHANISM:
+                write_addr = MASK_DOOR;
+                break;
+            default:
+                return;
+        }
+        HAL.setBits(write_addr);
+
+        for (int i = 0; i < TX_FRAME_SIZE; i++) {
+            HAL.clrBits(MASK_SCLK);
+            HAL.writeBits(MASK_DATA, 0x1 & data);
+
+            data >>= 1;
+            HAL.setBits(MASK_SCLK);
+        }
+
+        HAL.clrBits(MASK_DATA);
+        HAL.clrBits(MASK_SCLK);
+        HAL.clrBits(write_addr);
     }
 
     // retorna true se o canal série estiver ocupado
     public static boolean isBusy() {
-        return false;
+        return HAL.isBit(MASK_BUSY);
     }
 }
