73dc02a61a0587bd81f43b04f1c1b8d2504761ad
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-11-16 11:40:07 +0000

    Close KDB class

--- src/isel/leic/lic/g2/Keyboard/KBD.java
+++ src/isel/leic/lic/g2/Keyboard/KBD.java
@@ -1,8 +1,6 @@
 /*-
  * SPDX-License-Identifier: BSD-3-Clause
- *
  * Copyright (c) 2019 Luis Bandarra <luis.bandarra@homestudio.pt>
- * Copyright (c) 2019 Gonçalo Lourenço
  */
 
 /*
@@ -21,15 +19,15 @@
  *
  * O sinal recebido na camada HAL tem a dimensão de 1 byte (8 bits), pelo que
  * definimos para as várias máscaras os seguintes bits:
- *   - Dval (RX_VAL): 8bit - 0x80
- *   - Data (RX_DATA): primeiros 4 bits - 0xf
- *   - ACK (TX_ACK): 6bit - 0x20
+ *   - Dval (VAL): 8bit - 0x80
+ *   - Data (DATA): primeiros 4 bits - 0xf
+ *   - ACK (ACK): 6bit - 0x20
  *
- *   RX_VAL     RX_DATA
+ *   VAL     DATA
  *     1 0 0 0  1 1 1 1
  *
  *     0 0 0 0  0 0 0 1
- *        TX_ACK
+ *        ACK
  *
  * Apos receção do valor do teclado, é necessário mapear o valor para o carater
  * do enviado:
@@ -57,9 +55,9 @@ import isel.leic.lic.g2.HAL;
 import isel.leic.utils.Time;
 
 public class KBD {
-    private static int RX_VAL = 0x80;
-    private static int RX_DATA = 0xf;
-    private static int TX_ACK = 0x1;
+    private static int VAL = 0x80;
+    private static int DATA = 0xf;
+    private static int ACK = 0x1;
 
     public static final char NONE = 0;
     public static final char[][] MAP_CHAR = {
@@ -77,13 +75,13 @@ public class KBD {
     public static char getKey() {
         char key = NONE;
 
-        if (HAL.isBit(RX_VAL)) {
-            key = mapKeyToChar(HAL.readBits(RX_DATA));
-            HAL.setBits(TX_ACK);
-            while (HAL.isBit(RX_VAL)); // espera mudança RX_VAL
-            HAL.clrBits(TX_ACK);
-        }
+        if (HAL.isBit(VAL)) {
+            key = mapKeyToChar(HAL.readBits(DATA));
+            HAL.setBits(ACK);
+            while (HAL.isBit(VAL));
 
+            HAL.clrBits(ACK);
+        }
         return key;
     }
 
@@ -96,32 +94,10 @@ public class KBD {
         while ((Time.getTimeInMillis() < time) || (key == NONE)) {
             key = getKey();
         }
-
         return key;
     }
 
     private static char mapKeyToChar(int k) {
         return MAP_CHAR[k>>2][k & 0x3];
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Testes KDB");
-
-        System.out.println("Teste MAPEAR CARATER");
-        System.out.println("Teste 1 - mapKeyToChar (1): " + mapKeyToChar(0x0)); // 0000
-        System.out.println("Teste 2 - mapKeyToChar (4): " + mapKeyToChar(0x1)); // 0001
-        System.out.println("Teste 3 - mapKeyToChar (7): " + mapKeyToChar(0x2)); // 0010
-        System.out.println("Teste 4 - mapKeyToChar (*): " + mapKeyToChar(0x3)); // 0011
-        System.out.println("Teste 5 - mapKeyToChar (2): " + mapKeyToChar(0x4)); // 0100
-        System.out.println("Teste 6 - mapKeyToChar (5): " + mapKeyToChar(0x5)); // 0101
-        System.out.println("Teste 7 - mapKeyToChar (8): " + mapKeyToChar(0x6)); // 0110
-        System.out.println("Teste 8 - mapKeyToChar (0): " + mapKeyToChar(0x7)); // 0111
-        System.out.println("Teste 9 - mapKeyToChar (3): " + mapKeyToChar(0x8)); // 1000
-        System.out.println("Teste 10 - mapKeyToChar (6): " + mapKeyToChar(0x9)); // 1001
-        System.out.println("Teste 11 - mapKeyToChar (9): " + mapKeyToChar(0xa)); // 1010
-        System.out.println("Teste 12 - mapKeyToChar (#): " + mapKeyToChar(0x0b)); // 1011
-
-        System.out.println("Teste waitKey");
-        System.out.println("Teste 13 - waitKey (NONE) " + waitKey(1000));
     }
 }
