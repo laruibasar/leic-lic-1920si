d04bed92c87b61fa4f083c52e2088d6409939f71
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-12-14 10:51:00 +0000

    Add utils to read properties from file

--- src/isel/leic/lic/g2/Utils.java
+++ src/isel/leic/lic/g2/Utils.java
@@ -1,4 +1,19 @@
 package isel.leic.lic.g2;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Utils {
+    public static String getProperties(String property) {
+        try (InputStream io = new FileInputStream("USB_PORT.properties")) {
+            Properties prop = new Properties();
+
+            prop.load(io);
+            return prop.getProperty(property);
+        } catch (IOException ie) {
+            return null;
+        }
+    }
 }
