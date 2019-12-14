a642c97d9944f3f61eaaac7eb8730f264848e13d
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-12-14 19:51:11 +0000

    Added methods for more control

--- src/isel/leic/lic/g2/TUI/TUI.java
+++ src/isel/leic/lic/g2/TUI/TUI.java
@@ -25,32 +25,86 @@ public class TUI {
     // retorno EABORT significa utilizador cancelou o comando
     private static final int EABORT = -2;
 
+    private static final boolean CURSOR_SET = true;
+    private static final boolean CURSOR_OFF = false;
+
     // Inicia a classe, estabelecendo os valores iniciais
     public static void init() {
         KBD.init();
         LCD.setSerialInterface(true);
         LCD.init();
+        cursorSet(CURSOR_OFF);
     }
 
     // obtencao de input, preparando mensagem de introducao
-    public static int readInput(String txt, int ndigits, boolean obsfucate, char obs, long timeout, int lin, int col) {
-        showMessage(txt, lin, col);
-        int input_col = col + txt.length() + 1;
+    public static int readInput(String txt, int ndigits, boolean obsfucate, char obs, long timeout, int lin) {
+        showMessage(txt, lin, 0);
+        int input_col = txt.length();
 
         return readInteger(ndigits, obsfucate, obs, timeout, lin, input_col);
     }
 
     // apresentacao do tempo
-    public static void showCurrentDateTime(int lin, int col) {
+    public static void showCurrentDateTime(int lin) {
         LocalDateTime currentLocal = LocalDateTime.now();
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-        showMessage(currentLocal.format(format), lin, col);
+        showMessage(currentLocal.format(format), lin, 0);
+    }
+
+    // leitura de um carater
+    public static char readInputKeyboard() {
+        return KBD.getKey();
+    }
+
+    // apresentacao de mensagem, no ultimo local de cursor
+    public static void showMessage(String msg) {
+        LCD.write(msg);
+    }
+
+    // apresentacao de mensagem, na posicao pretendida
+    public static void showMessage(String msg, int lin, int col) {
+        LCD.cursor(lin, col);
+        cursorSet(CURSOR_OFF);
+        showMessage(msg);
+    }
+
+    // apresentacao de caracter
+    public static void showCharacter(char c, int lin, int col) {
+        showMessage(String.valueOf(c), lin, col);
+    }
+
+    // apresentacao de mensagem, com indicacao de centrada
+    public static void showMessage(String msg, int lin, boolean center) {
+        int col = 0;
+        if (center)
+            col = (LCD.COLS - msg.length()) / 2;
+
+        showMessage(msg, lin, col);
+    }
+
+    // limpar ecra
+    public static void clearScreen() {
+        LCD.clear();
+        cursorSet(CURSOR_OFF);
+    }
+
+    // limpar linha, verificar se o flicker é demasiado visivel com o eng.
+    public static void clearLine(int lin) {
+        char[] clear = new char[LCD.COLS];
+        for (int i = 0; i < LCD.COLS; i++) {
+            clear[i] = ' ';
+        }
+        showMessage(String.copyValueOf(clear), 1, 0);
+        cursorSet(CURSOR_OFF);
+    }
+
+    public static void cursorSet(boolean set) {
+        LCD.cursorSet(set);
     }
 
     // leitura de um numero, controlando a apresentacao do input do utilizador
-
-    public static int readInteger(int ndigits, boolean obsfucate, char obs, long timeout, int lin, int col) {
+    private static int readInteger(int ndigits, boolean obsfucate, char obs, long timeout, int lin, int col) {
         int value = 0;
         int i = 0;
         showInputRequest(ndigits, obsfucate, obs, lin, col);
@@ -86,11 +140,6 @@ public class TUI {
         return value;
     }
 
-    // leitura de um carater
-    public static char readInputKeyboard() {
-        return KBD.getKey();
-    }
-
     private static void showInputRequest(int ndigits, boolean obsfucate, char obs, int lin, int col) {
         if (obsfucate) {
             String msg = "";
@@ -99,33 +148,8 @@ public class TUI {
             }
             showMessage(msg, lin, col);
             LCD.cursor(lin, col);
+            cursorSet(CURSOR_SET);
         }
-    }
-
-    // apresentacao de mensagem
-    public static void showMessage(String msg, int lin, int col) {
-        LCD.cursor(lin, col);
-        LCD.write(msg);
-    }
-
-    // apresentacao de mensagem, com indicacao de centrada
-    public static void showMessage(String msg, int lin, boolean center) {
-        int col = 0;
-        if (center)
-            col = (LCD.COLS - msg.length()) / 2;
-
-        showMessage(msg, lin, col);
-    }
-
-    // apresentacao de caracter
-    public static void showCharacter(char c, int lin, int col) {
-        LCD.cursor(lin, col);
-        LCD.write(c);
-    }
-
-    // limpar ecra
-    public static void clearScreen() {
-        LCD.clear();
     }
 
     public static void main(String[] args) {
@@ -154,20 +178,20 @@ public class TUI {
 
         System.out.println("3 - Teste ler User");
         System.out.println("Pedir utilizador (introduzir 123)");
-        int user_test = readInput(USER, USER_DIGITS, true, '?', 5000, 1, 0);
+        int user_test = readInput(USER, USER_DIGITS, true, '?', 5000, 1);
         System.out.println("Resultado: " + user_test);
 
         clearScreen();
 
         System.out.println("4 - Teste ler passwd");
         System.out.println("pedir password: (introduzir 1234)");
-        int pin_test = readInput(PIN, PIN_DIGITS, true, '?', 5000, 1, 0);
+        int pin_test = readInput(PIN, PIN_DIGITS, true, '?', 5000, 1);
         System.out.println("Resultado: " + pin_test);
 
         clearScreen();
 
         System.out.println("5 - Teste apresentacao tempo");
-        showCurrentDateTime(0, 0);
+        showCurrentDateTime(0);
         Time.sleep(5000);
 
         System.out.println();
