43cd32e3f38634173603dbc472cf97c89e7b778c
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-12-14 10:52:58 +0000

    Refactor code to avoid code repeat

--- src/isel/leic/lic/g2/Door/DoorMechanism.java
+++ src/isel/leic/lic/g2/Door/DoorMechanism.java
@@ -2,11 +2,24 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * Copyright (c) 2019 Luis Bandarra <luis.bandarra@homestudio.pt>
  */
+
+/*
+ * Classe para controlo do mecanismo da porta.
+ * Podemos pedir para abrir ou fechar a porta, enviando a velocidade com que queremos
+ * que seja a manobra da porta.
+ *
+ * O sinal de dados para o SerialEmitter vai ser:
+ *          7 6 5   4    3    2    1   0
+ * Output:  0 0 0 (D3) (D2) (D1) (D0) (OC)
+ *
+ * Acordo com a especificao projeto
+ */
 package isel.leic.lic.g2.Door;
 
 import isel.leic.lic.g2.SerialEmitter;
 
-public class DoorMechanism {    // Controla o estado do mecanismo de abertura da porta
+// Controla o estado do mecanismo de abertura da porta
+public class DoorMechanism {
     private static final int MASK_DATA_SIZE = 0xf;
     private static final int DOOR_OPEN = 1;
     private static final int DOOR_CLOSE = 0;
@@ -21,20 +34,18 @@ public class DoorMechanism {    // Controla o estado do mecanismo de abertura da
     // O sinal de dados para o SerialEmitter vai ser:
     // 0 0 0 (V3) (V2) (V1) (V0) (OC) de acordo com a especificao projeto
     public static void open(int velocity) {
-        int send = ((MASK_DATA_SIZE & velocity) << 1) | DOOR_OPEN;
-
-        while (!finished());
-        SerialEmitter.send(SerialEmitter.Destination.DOOR_MECHANISM, send);
+        send(((MASK_DATA_SIZE & velocity) << 1) | DOOR_OPEN);
     }
 
     // Envia comando para fechar a porta, com o parameto de velocidade
-    // O sinal de dados para o SerialEmitter vai ser:
-    // 0 0 0 (D3) (D2) (D1) (D0) (OC) de acordo com a especificao projeto
     public static void close(int velocity) {
-        int send = ((MASK_DATA_SIZE & velocity) << 1) | DOOR_CLOSE;
+        send( ((MASK_DATA_SIZE & velocity) << 1) | DOOR_CLOSE);
+    }
 
+    // Envia o comando para a porta
+    private static void send(int cmd) {
         while (!finished());
-        SerialEmitter.send(SerialEmitter.Destination.DOOR_MECHANISM, send);
+        SerialEmitter.send(SerialEmitter.Destination.DOOR_MECHANISM, cmd);
     }
 
     // Verifica se o comando anterior está concluido
